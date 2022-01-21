2563611d15fba7193a0f5dbaee4aa0a070ca878d
Vitalii Kalancha <vitalii_kalancha@epam.com>
2022-01-21 16:51:57 +0200

    add security groups

--- code.tf
+++ code.tf
@@ -9,3 +9,32 @@ resource "aws_s3_bucket" "tf_course" {
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name = "prod_web"
+  description = "Allow standaty http snd https ports"
+
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+  } 
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
